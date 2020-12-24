883f55d3c5131d7f6162ed23bced4aa3137bfd51
Michael Penn <mwpenn94@gmail.com>
2020-12-24 04:14:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,7 @@ def reverse_each_word(string)
     string_arrayed << string.split
     string_arrayed.each {|word| return word.reverse}
 end
+
+def test(sentence)
+  sentence.split.map {|word| word.reverse}.join(" ")
+end
