d523a3c2a379c8dd6c1f7d1c8f28a7336c3665c8
Michael Penn <mwpenn94@gmail.com>
2020-12-24 04:15:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse_each_word(string)
-    string_arrayed = []
-    string_arrayed << string.split
-    string_arrayed.each {|word| return word.reverse}
-end
-
-def test(sentence)
-  sentence.split.map {|word| word.reverse}.join(" ")
+   string.split.map {|word| word.reverse}.join(" ") 
 end
