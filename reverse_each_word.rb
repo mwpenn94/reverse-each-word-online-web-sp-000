31c8aa80b0eace970a895d485dfb89998198948a
Michael Penn <mwpenn94@gmail.com>
2020-12-24 04:12:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
     string_arrayed = []
     string_arrayed << string.split
-    string_arrayed.collect {|word| word.reverse}
+    string_arrayed.each {|word| return word.reverse}
 end
